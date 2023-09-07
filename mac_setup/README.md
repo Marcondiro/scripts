0c5a48c467648eb309edacdc61484ae27cc6fa81
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2023-09-07 22:01:50 +0200

    feat(mac_setup): add mac setup

---
+++ mac_setup/README.md
@@ -0,0 +1,6 @@
+# Mac setup
+
+Run:
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Marcondiro/scripts/main/mac_setup/setup.sh)"
+```
---
+++ mac_setup/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+# Pop-up the installer for command line developers tools
+git -v >/dev/null
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add brew to path and reload profile
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+eval "\$(/opt/homebrew/bin/brew shellenv)"
+
+# Install brew formulae
+/bin/bash -c "curl -fsSL https://raw.githubusercontent.com/Marcondiro/scripts/main/mac_setup/brew_formulaes | xargs brew install"
+
+# Install brew casks
+/bin/bash -c "curl -fsSL https://raw.githubusercontent.com/Marcondiro/scripts/main/mac_setup/brew_casks | xargs brew install --cask"
+
+# Install apps from Macapp store
+# (not available in hombrew at the time of writing)
+
+#                         Wireguard  iMovie
+/bin/bash -c "mas install 1451685025 408981434"
